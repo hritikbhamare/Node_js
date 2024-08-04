7648346c65ae6215a2e7ac9101446bab61c5cf5d
Hritik Bhamare <80049008+hritikbhamare@users.noreply.github.com>
2024-08-04 17:23:53 +0000

    conersion

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,8 @@
+let accountEmail = "bhamare2001@gmail.com"
+let accountPassword = "Pass@123"
+accountCity = "Mumbai"
+
+
+console.log(accountPassword);
+console.table([accountEmail, accountPassword, accountCity])
+
